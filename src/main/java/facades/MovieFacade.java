2a496d37ae4ae49c98c44afa882fc5bcd1a5f74e
Simon Kristopher Kruse Bentzen <simonkruse2@gmail.com>
2019-09-04 10:58:24 +0200

    movie

---
+++ src/main/java/facades/MovieFacade.java
@@ -0,0 +1,112 @@
+package facades;
+
+import entities.Movie;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.TypedQuery;
+import utils.EMF_Creator;
+
+/**
+ *
+ * Rename Class to a relevant name Add add relevant facade methods
+ */
+public class MovieFacade {
+
+    private static MovieFacade instance;
+    private static EntityManagerFactory emf;
+
+    //Private Constructor to ensure Singleton
+    private MovieFacade() {
+    }
+
+    /**
+     *
+     * @param _emf
+     * @return an instance of this facade class.
+     */
+    public static MovieFacade getMovieFacade(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new MovieFacade();
+        }
+        return instance;
+    }
+
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public Movie getMovieByID(int id) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            return em.find(Movie.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Movie> getAllMovies() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            TypedQuery query = em.createQuery("SELECT m FROM movie m", Movie.class);
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Movie> getMovieByTitle(String title) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            TypedQuery query = em.createQuery("SELECT m FROM movie m WHERE title =: title", Movie.class)
+                    .setParameter("title", title);
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Movie> getMovieByYear(int year) {
+        EntityManager em = emf.createEntityManager();
+        try {
+            TypedQuery query = em.createQuery("SELECT m FROM movie m WHERE year=: year", Movie.class)
+                    .setParameter("year", year);
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Movie addMovie(int year, String name, String[] actors) {
+        EntityManager em = emf.createEntityManager();
+        Movie m = new Movie(year, name, actors);
+        try {
+            em.getTransaction().begin();
+            em.persist(m);
+            em.getTransaction().commit();
+            return m;
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getNumberOfMovies() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            TypedQuery q = em.createQuery("select m from Movie m", Movie.class);
+            return q.getResultList().size();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = EMF_Creator.createEntityManagerFactory(EMF_Creator.DbSelector.DEV, EMF_Creator.Strategy.CREATE);
+        MovieFacade mf = MovieFacade.getMovieFacade(emf);
+        String[] actors = {"Vin Diesel", "Lektor Blomme"};
+        System.out.println(mf.addMovie(1998, "Det forsømte forår", actors));
+        System.out.println(mf.addMovie(2087, "The Fast and the Furios 235", actors));
+        System.out.println(mf.getMovieByID(1));
+    }
+}
---
+++ src/main/java/rest/MovieResource.java
@@ -0,0 +1,90 @@
+package rest;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entities.Movie;
+import utils.EMF_Creator;
+import facades.MovieFacade;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("movie")
+public class MovieResource {
+
+    private static final EntityManagerFactory EMF = EMF_Creator.createEntityManagerFactory(
+                "pu",
+                "jdbc:mysql://localhost:3307/movie",
+                "dev",
+                "ax2",
+                EMF_Creator.Strategy.CREATE);
+    private static final MovieFacade FACADE =  MovieFacade.getMovieFacade(EMF);
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+            
+    @GET
+    @Produces({MediaType.APPLICATION_JSON})
+    public String demo() {
+        return "{\"msg\":\"Hello World\"}";
+    }
+
+
+    @POST
+    @Consumes({MediaType.APPLICATION_JSON})
+    public void create(Movie entity) {
+        throw new UnsupportedOperationException();
+    }
+    
+    @GET
+    @Path("all")
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getAllMovies() {
+        List<Movie> movies = FACADE.getAllMovies();
+        return GSON.toJson(movies);
+    }
+    
+//    @POST
+//    @Path("/{title}/{releaseYear}")
+//    @Consumes({MediaType.APPLICATION_JSON})
+//    @Produces({MediaType.APPLICATION_JSON})
+//    public String createMovie(@PathParam("title") String title, @PathParam("releaseYear") int releaseYear){
+//        return  GSON.toJson(FACADE.addMovie(title, releaseYear));
+//    }
+    
+    @GET
+    @Path("title/{title}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getMovieByTitle(@PathParam("title") String title) {
+     return GSON.toJson(FACADE.getMovieByTitle(title));   
+    }
+    
+    @GET
+    @Path("count")
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getNumberOfMovies() {
+        return "{\"count\":\"" + FACADE.getNumberOfMovies() + "\"}";
+    }
+    
+    @GET
+    @Path("Year/{Year}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getMovieByReleaseYear(@PathParam("Year") int Year) {
+     return GSON.toJson(FACADE.getMovieByYear(Year));   
+    }
+    
+    @GET
+    @Path("/{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public String getMovieById(@PathParam("id") int id) {
+        return GSON.toJson(FACADE.getMovieByID(id));
+        
+    }
+   
+}
